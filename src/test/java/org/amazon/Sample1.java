c0ab99588d0d07ba459b53a980f6704d9fb3d5d5
PrathibhaNair <prathibha.nair25@gmail.com>
2020-07-19 10:41:30 +0530

    New updates by Evi!

--- src/test/java/org/amazon/Sample1.java
+++ src/test/java/org/amazon/Sample1.java
@@ -5,5 +5,8 @@ public class Sample1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Amazon!");
+		
+		System.out.println("New lines of code added by Evi!");
+		System.out.println("Check out new products at Amazon!");
 	}
 }
